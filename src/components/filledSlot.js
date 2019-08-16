3c6cc4aceacc54905631b9c8b78963ab978204e4
Bryan Fong <bryanfong.dev@gmail.com>
2019-08-16 19:02:05 -0400

    change maxlength of form inputs

--- src/components/filledSlot.js
+++ src/components/filledSlot.js
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const FilledSlot = ({ index, brand, style, size, upcID, editInfo, deleteInfo }) => (
+
   <div className='filled' >
-    <p><span className="label">Brand : </span>{brand}</p>
-    <p><span className="label">Style : </span>{style}</p>
-    <p><span className="label">Size : </span>{size}</p>
-    <p><span className="label">UPC ID : </span>{upcID}</p>
+    <h4>Brand</h4>
+    <p className="info">{brand || <br />}</p >
+    <h4>Style</h4>
+    <p className="info">{style || <br />}</p >
+    <h4>Size</h4>
+    <p className="info">{size || <br />}</p >
+    <h4>UPC ID</h4>
+    <p className="info">{upcID || <br />}</p >
     <div className="buttons">
       <button onClick={() => editInfo(index)}>Edit</button>
       <button onClick={() => deleteInfo(index)}>Delete</button>
--- src/components/form.js
+++ src/components/form.js
@@ -4,13 +4,13 @@ const Form = ({ index, brand, style, size, upcID, submitInfo }) => (
   <div>
     <form className='form' onSubmit={(e) => submitInfo(e, index)}>
       <label htmlFor="brand"> Brand</label>
-      <input id="brand" name="brand" type="text" defaultValue={brand} />
+      <input id="brand" name="brand" type="text" defaultValue={brand} maxlength="50" />
       <label htmlFor="style"> Style </label>
-      <input id="style" name="style" type="text" defaultValue={style} />
+      <input id="style" name="style" type="text" defaultValue={style} maxlength="50" />
       <label htmlFor="size"> Size </label>
-      <input id="size" name="size" type="text" defaultValue={size} />
+      <input id="size" name="size" type="text" defaultValue={size} maxlength="10" />
       <label htmlFor="upcID"> UPC ID </label>
-      <input id="upcID" name="upcID" type="text" defaultValue={upcID} />
+      <input id="upcID" name="upcID" type="text" defaultValue={upcID} maxlength="12" />
       <input className="submitButton" type="submit" value="Submit"></input>
     </form>
   </div>
